7796e3c4b3a13ff59aa8479fee318430efdc7dfa
Agata Grzybowska <agata.k.grzybowska@gmail.com>
2021-10-23 22:39:03 +0000

    testy formatera

--- test/test_formater.py
+++ test/test_formater.py
@@ -1,11 +1,14 @@
-from hello_world.formater import plain_text_upper_case
+from hello_world.formater import plain_text_upper_case, plain_text_lower_case
 import unittest
 
 
 class TestFormater(unittest.TestCase):
     def test_plain_uppercase(self):
-        r = plain_text_upper_case("wwww", "EEEMSG")
-        name = r.split(" ")[0]
-        msg = r.split(" ")[1]
-        self.assertTrue(name.isupper())
-        self.assertTrue(msg.isupper())
+        r = plain_text_upper_case("wwww", "eeemsg")
+
+        self.assertEqual('EEEMSG WWWW', r)
+
+    def test_plain_text_lower_case(self):
+        r = plain_text_lower_case("WWWW", "EEEMSG")
+
+        self.assertEqual('eeemsg wwww', r)
